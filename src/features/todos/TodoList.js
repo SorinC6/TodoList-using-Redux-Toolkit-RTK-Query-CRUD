0423c8bae81a526a1694dd822c804828369b29a2
SorinC6 <kish.sorin@yahoo.com>
2022-09-07 17:43:04 +0300

    add api hooks in component

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -3,18 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
 
-import { useGetTodosQuery } from '../api/apiSlice'
+import { useGetTodosQuery, useAddTodoMutation, useUpdateTodoMutation, useDeleteTodoMutation } from '../api/apiSlice'
 
 const TodoList = () => {
     const [newTodo, setNewTodo] = useState('')
 
     const { data: todos, isLoading, isSuccess, isError, error } = useGetTodosQuery()
+    const [addTodo] = useAddTodoMutation()
+    const [updateTodo] = useUpdateTodoMutation()
+    const [deleteTodo] = useDeleteTodoMutation()
 
     console.log({ data: todos, isLoading, isSuccess, isError, error })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        //addTodo
+        addTodo({ userId: 1, title: newTodo, completed: false })
         setNewTodo('')
     }
 
@@ -41,6 +44,7 @@ const TodoList = () => {
         content = <p>Loading...</p>
     } else if (isSuccess) {
         content = todos.map(todo => { //JSON.stringify(todos)
+            console.log("todo.id", todo.id)
             return (
                 <article key={todo.id}>
                     <div className="todo">
@@ -48,13 +52,13 @@ const TodoList = () => {
                             type="checkbox"
                             checked={todo.completed}
                             id={todo.id}
-                        // onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
                         />
                         <label htmlFor={todo.id}>{todo.title}</label>
                     </div>
-                    {/* <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+                    <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
                         <FontAwesomeIcon icon={faTrash} />
-                    </button> */}
+                    </button>
                 </article>
             )
         })
